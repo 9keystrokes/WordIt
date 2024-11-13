6338aae338011d25f0d9a99face50305b08ef4a1
9keystrokes <168451989+9keystrokes@users.noreply.github.com>
2024-11-13 10:45:35 +0530

    Add files via upload

--- my-project/src/component/CreatePost.js
+++ my-project/src/component/CreatePost.js
@@ -17,7 +17,7 @@ export default function CreatePost() {
     setTitle("");
     setParagraph("");
     axios
-      .post("http://localhost:5000/posts/create", newArticle)
+      .post("https://wordit-p7q2.onrender.com/posts/create", newArticle)
       .then((res) => {
         console.log(res.data);
         setMessage("Article Added Successfully");
